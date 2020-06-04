45f8e5a9797ca8525feee51673cd58b032039c66
Vladimir <avevanguard@gmail.com>
2020-06-04 18:24:36 +0300

    added clicks events on like && stars

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -9,6 +9,13 @@ import PostAddForm from '../post-add-form/post-add-form';
 import './app.css';
 
 const App = () => {
+
+    const data = [
+        {label: 'Going to learn React', important: true, id: 'qwe'},
+        {label: 'Thats so good', important: false, id: 'rew'},
+        {label: 'I need a break...', important: false, id: 'qert'},
+    ];
+
     return(
         <div className = "app">
             <AppHeader/>
@@ -16,7 +23,7 @@ const App = () => {
                     <SearchPanel/>
                     <PostStatusFilter/>
             </div>
-            <PostList/>
+            <PostList posts={data}/>
             <PostAddForm/> 
         </div>
     )
